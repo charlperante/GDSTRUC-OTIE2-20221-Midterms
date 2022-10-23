131cce6e18038feafc3204aea2e8c56b175d0d8b
Charl Perante <mariecharlize.perante@benilde.edu.ph>
2022-10-24 05:23:13 +0800

    Added comments

--- GDSTRUC-Midterms/src/Main.java
+++ GDSTRUC-Midterms/src/Main.java
@@ -3,16 +3,16 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        CardStack deck = new CardStack();
-        CardStack player = new CardStack();
-        CardStack discarded = new CardStack();
+        CardStack deck = new CardStack(); // Card deck
+        CardStack player = new CardStack(); // Player hand
+        CardStack discarded = new CardStack(); // Discarded pile
 
-        int min = 1;
-        int max = 5;
-        int deckCount = 30;
-        int playerCount = 0;
-        int pileCount = 0;
-        int i = 1;
+        int min = 1; // Minimum x value
+        int max = 5; // Maximum x value
+        int deckCount = 30; // Card deck counter
+        int playerCount = 0; // Player hand counter
+        int pileCount = 0; // Discarded pile counter
+        int i = 1; // Round counter
 
         deck.push(new Cards("Phil"));       deck.push(new Cards("Claire"));         // 1, 2
         deck.push(new Cards("Luke"));       deck.push(new Cards("Haley"));          // 3, 4
@@ -30,38 +30,39 @@ public class Main {
         deck.push(new Cards("Will"));       deck.push(new Cards("Lucas"));          // 27, 28
         deck.push(new Cards("Dustin"));     deck.push(new Cards("Max"));            // 29, 30
 
-        int randX4 = (int)Math.floor(Math.random()*(max-min+1)+min);
+        int randX4 = (int)Math.floor(Math.random()*(max-min+1)+min); // Random x value
 
+        // Round 1
         System.out.println("------------------------------");
         System.out.println("ROUND 1: Draw " + randX4 + " card/s");
         System.out.println();
-        if (randX4 == 1) {
+        if (randX4 == 1) { // if x is equals to 1
             player.push(deck.pop());
             playerCount++;
             deckCount--;
         }
-        if (randX4 == 2) {
+        else if (randX4 == 2) { // if x is equals to 2
             for (int p = 0; p < 2; p++) {
                 player.push(deck.pop());
                 playerCount++;
                 deckCount--;
             }
         }
-        if (randX4 == 3) {
+        else if (randX4 == 3) { // if x is equals to 3
             for (int p = 0; p < 3; p++) {
                 player.push(deck.pop());
                 playerCount++;
                 deckCount--;
             }
         }
-        if (randX4 == 4) {
+        else if (randX4 == 4) { // if x is equals to 4
             for (int p = 0; p < 4; p++) {
                 player.push(deck.pop());
                 playerCount++;
                 deckCount--;
             }
         }
-        if (randX4 == 5) {
+        else if (randX4 == 5) { // if x is equals to 5
             for (int p = 0; p < 5; p++) {
                 player.push(deck.pop());
                 playerCount++;
@@ -69,6 +70,7 @@ public class Main {
             }
         }
 
+        // Console display
         System.out.println("Player Hand: " + playerCount);
         player.printStack();
         System.out.println();
@@ -76,6 +78,7 @@ public class Main {
         System.out.println("Discarded Pile: " + pileCount);
         System.out.println();
 
+        // User Input
         System.out.print("Press Enter key to continue...");
         try
         {
@@ -84,55 +87,55 @@ public class Main {
         catch(Exception e)
         {}
 
-        while (!deck.isEmpty()) {
-            i++;
+        while (!deck.isEmpty()) { // Card deck stack is not empty
+            i++; // Round counter
 
             System.out.println("------------------------------");
             System.out.print("ROUND " + i + ": ");
 
-            int minC = 1;
-            int maxC = 3;
+            int minC = 1; // Minimum c value
+            int maxC = 3; // Maximum c value
 
-            int randC = (int)Math.floor(Math.random()*(maxC-minC+1)+minC);
+            int randC = (int)Math.floor(Math.random()*(maxC-minC+1)+minC); // Random c value for randomized program
 
-            if (randC == 1) {
+            if (randC == 1) { // Draw x cards program
 
-                int randX1 = (int)Math.floor(Math.random()*(max-min+1)+min);
+                int randX1 = (int)Math.floor(Math.random()*(max-min+1)+min); // Random x value
 
-                if (randX1 > deckCount) {
-                    randX1 = deckCount;
+                if (randX1 > deckCount) { // If x value is greater than number of cards in card deck
+                    randX1 = deckCount; // then x value is equals to number of cards in card deck
                 }
 
                 System.out.println("Draw " + randX1 + " card/s");
                 System.out.println();
 
-                if (randX1 == 1 && deckCount >= 1) {
+                if (randX1 == 1 && deckCount >= 1) { // if x is equals to 1
                     player.push(deck.pop());
                     playerCount++;
                     deckCount--;
                 }
-                else if (randX1 == 2 && deckCount >= 2) {
+                else if (randX1 == 2 && deckCount >= 2) { // if x is equals to 2
                     for (int p = 0; p < 2; p++) {
                         player.push(deck.pop());
                         playerCount++;
                         deckCount--;
                     }
                 }
-                else if (randX1 == 3 && deckCount >= 3) {
+                else if (randX1 == 3 && deckCount >= 3) { // if x is equals to 3
                     for (int p = 0; p < 3; p++) {
                         player.push(deck.pop());
                         playerCount++;
                         deckCount--;
                     }
                 }
-                else if (randX1 == 4 && deckCount >= 4) {
+                else if (randX1 == 4 && deckCount >= 4) { // if x is equals to 4
                     for (int p = 0; p < 4; p++) {
                         player.push(deck.pop());
                         playerCount++;
                         deckCount--;
                     }
                 }
-                else if (randX1 == 5 && deckCount >= 5) {
+                else if (randX1 == 5 && deckCount >= 5) { // if x is equals to 5
                     for (int p = 0; p < 5; p++) {
                         player.push(deck.pop());
                         playerCount++;
@@ -141,44 +144,44 @@ public class Main {
                 }
             }
 
-            if (randC == 2 && !player.isEmpty()) {
+            if (randC == 2 && !player.isEmpty()) { // Discard x cards program
 
-                int randX2 = (int)Math.floor(Math.random()*(max-min+1)+min);
+                int randX2 = (int)Math.floor(Math.random()*(max-min+1)+min); // Random x value
 
-                if (randX2 > playerCount) {
-                    randX2 = playerCount;
+                if (randX2 > playerCount) { // If x value is greater than number of cards in player hand
+                    randX2 = playerCount; // then x value is equals to number of cards in player hand
                 }
 
                 System.out.println("Discard " + randX2 + " card/s");
                 System.out.println();
 
-                if(randX2 == 1 && playerCount >= 1) {
+                if(randX2 == 1 && playerCount >= 1) { // if x is equals to 1
                     discarded.push(player.pop());
                     playerCount--;
                     pileCount++;
                 }
-                else if(randX2 == 2 && playerCount >= 2) {
+                else if(randX2 == 2 && playerCount >= 2) { // if x is equals to 2
                     for (int d = 0; d < 2; d++){
                         discarded.push(player.pop());
                         playerCount--;
                         pileCount++;
                     }
                 }
-                else if(randX2 == 3 && playerCount >= 3) {
+                else if(randX2 == 3 && playerCount >= 3) { // if x is equals to 3
                     for (int d = 0; d < 3; d++){
                         discarded.push(player.pop());
                         playerCount--;
                         pileCount++;
                     }
                 }
-                else if(randX2 == 4 && playerCount >= 4) {
+                else if(randX2 == 4 && playerCount >= 4) { // if x is equals to 4
                     for (int d = 0; d < 4; d++){
                         discarded.push(player.pop());
                         playerCount--;
                         pileCount++;
                     }
                 }
-                else if(randX2 == 5 && playerCount >= 5) {
+                else if(randX2 == 5 && playerCount >= 5) { // if x is equals to 5
                     for (int d = 0; d < 5; d++){
                         discarded.push(player.pop());
                         playerCount--;
@@ -186,62 +189,62 @@ public class Main {
                     }
                 }
             }
-            else if (randC == 2 && player.isEmpty()){
+            else if (randC == 2 && player.isEmpty()){ // if there are no cards in player hand
                 System.out.println("Not enough cards to discard from player hand. Try Again");
                 System.out.println();
             }
 
-            if (randC == 3 && !discarded.isEmpty()) {
+            if (randC == 3 && !discarded.isEmpty()) { // Get x cards from discarded pile program
 
-                int randX3 = (int)Math.floor(Math.random()*(max-min+1)+min);
+                int randX3 = (int)Math.floor(Math.random()*(max-min+1)+min); // Random x value
 
-                if (randX3 > pileCount) {
-                    randX3 = pileCount;
+                if (randX3 > pileCount) { // If x value is greater than number of cards in discarded pile
+                    randX3 = pileCount; // then x value is equals to number of cards in discarded pile
                 }
 
                 System.out.println("Get " + randX3 + " card/s from the discarded pile");
                 System.out.println();
 
-                if (randX3 == 1 && pileCount >= 1) {
+                if (randX3 == 1 && pileCount >= 1) { // if x is equals to 1
                     player.push(discarded.pop());
                     playerCount++;
                     pileCount--;
                 }
-                else if (randX3 == 2 && pileCount >= 2) {
+                else if (randX3 == 2 && pileCount >= 2) { // if x is equals to 2
                     for (int g = 0; g < 2; g++) {
                         player.push(discarded.pop());
                         playerCount++;
                         pileCount--;
                     }
                 }
-                else if (randX3 == 3 && pileCount >= 3) {
+                else if (randX3 == 3 && pileCount >= 3) { // if x is equals to 3
                     for (int g = 0; g < 3; g++) {
                         player.push(discarded.pop());
                         playerCount++;
                         pileCount--;
                     }
                 }
-                else if (randX3 == 4 && pileCount >= 4) {
+                else if (randX3 == 4 && pileCount >= 4) { // if x is equals to 4
                     for (int g = 0; g < 4; g++) {
                         player.push(discarded.pop());
                         playerCount++;
                         pileCount--;
                     }
                 }
-                else if (randX3 == 5 && pileCount >= 5) {
-                    for (int g = 0; g < 5
-                            ; g++) {
+                else if (randX3 == 5 && pileCount >= 5) { // if x is equals to 5
+                    for (int g = 0; g < 5; g++) {
                         player.push(discarded.pop());
                         playerCount++;
                         pileCount--;
                     }
                 }
             }
-            else if (randC == 3 && discarded.isEmpty()){
+            else if (randC == 3 && discarded.isEmpty()){ // if there are no cards in discarded pile
                 System.out.println("Not enough cards to get from discarded pile. Try Again");
                 System.out.println();
             }
 
+            // Display console
             System.out.println("Player Hand: " + playerCount);
             player.printStack();
             System.out.println();
@@ -249,6 +252,7 @@ public class Main {
             System.out.println("Discarded Pile: " + pileCount);
             System.out.println();
 
+            // User input
             System.out.print("Press Enter key to continue...");
             try
             {
@@ -257,7 +261,8 @@ public class Main {
             catch(Exception e)
             {}
         }
-        if (deck.isEmpty()) {
+
+        if (deck.isEmpty()) { // Card deck stack is empty
             System.out.println("------------------------------");
             System.out.println();
             System.out.println("CARD DECK IS EMPTY!");
